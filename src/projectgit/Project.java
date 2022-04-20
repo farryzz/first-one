d44c73231aed8243c51c44b290d128f161c307a4
farryzz <farry0315@gmail.com>
2022-04-20 17:55:16 +0200

    commit antes

---
+++ src/projectgit/Project.java
@@ -0,0 +1,11 @@
+package projectgit;
+
+public class Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+		System.out.println("quack");
+	}
+
+}
